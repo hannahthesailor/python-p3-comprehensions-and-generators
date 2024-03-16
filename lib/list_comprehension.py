a926c20dfebcd00db3d8b91c65facf5a8994eb07
Hannah Bradley <hannahthesailor@yahoo.com>
2024-03-16 01:49:47 -0400

    python-p3-comprehensions-and-generators.md

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = []
+    for num in num_list:
+        if num % 2 == 0:
+            even_numbers.append(num)
+    return even_numbers
+    
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
